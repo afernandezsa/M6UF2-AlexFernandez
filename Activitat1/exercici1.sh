fce45de33138ddf7875eda3c604c22d5053da3c4
afernandezsa <156780553+afernandezsa@users.noreply.github.com>
2024-01-29 10:49:37 +0100

    Create exercici1.sh

---
+++ Activitat1/exercici1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "\$HOME: El home del usuari és: $HOME"
+echo "\$PATH: Directori on el shell busca: $PATH"
+echo "\$SHELL: La shell utilitzada és: $SHELL"
+echo "\$USER: L'usuari és: $USER"
+echo "\$PWD: Ens trobem a: $PWD"
+echo "\$?: Mostra la última  funció: $?"
+echo "\$$: Mostra el PID: $$"
+
+#La barra '\' fa que es mostri per pantalla, després mostro variable globals
