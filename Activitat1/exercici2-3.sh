00e01027baf178e9d5f658df31b9ad8588b1387e
alexfernandez <156780553+afernandezsa@users.noreply.github.com>
2024-01-30 14:33:16 +0100

    Create exercici2-3.sh

---
+++ Activitat1/exercici2-3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+a1="$1"
+a2="$2"
+a3="$3"
+a4="$4"
+a5="$5"
+echo $* #Sense cometes, mostra els arguments tots junts com un simple string
+echo $@ #Sense cometes, interpreta els arguments amb espais
