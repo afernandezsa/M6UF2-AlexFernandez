9867cb7891d305e1dd43050895029e2e8ae8e1f8
alexfernandez <156780553+afernandezsa@users.noreply.github.com>
2024-01-30 14:17:24 +0100

    Update exercici2-1.sh

--- Activitat1/exercici2-1.sh
+++ Activitat1/exercici2-1.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+a1="$1"
+a2="$2"
+a3="$3"
+a4="$4"
+a5="$5"
+echo "$a1, $a2, $a3, $a4, $a5"
+echo "Nom del script: $0" #"$0" mostra el nom del arxiu que estem editant
+#Això farà que a l'hora de voler executar l'script hagi d'escriure les variables
