a00b3e8451c670e3c5927227654add67a6c1b064
alexfernandez <156780553+afernandezsa@users.noreply.github.com>
2024-01-30 14:32:59 +0100

    Delete Activitat1/ex2-3.sh

--- Activitat1/ex2-3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-a1="$1"
-a2="$2"
-a3="$3"
-a4="$4"
-a5="$5"
-echo $* #Sense cometes, mostra els arguments tots junts com un simple string
-echo $@ #Sense cometes, interpreta els arguments amb espais
