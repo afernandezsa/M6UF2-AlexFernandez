9fcb4ebf00ad2e404352f49bb37c5537990088a9
alexfernandez <156780553+afernandezsa@users.noreply.github.com>
2024-01-30 14:22:21 +0100

    Create exercici2-2.sh

---
+++ Activitat1/exercici2-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+a1="$1"
+a2="$2"
+a3="$3"
+a4="$4"
+a5="$5"
+echo "$a1"
+echo "$a2"
+echo "$a3"
+echo "$a4"
+echo "$a5"
+echo "s'han introduit $# caràcters" #"$#" compta els arguments
