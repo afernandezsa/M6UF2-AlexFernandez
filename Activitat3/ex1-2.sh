1176e19a25a80e0aed6587800fff69e793b4b8d6
alexfernandez <156780553+afernandezsa@users.noreply.github.com>
2024-02-06 20:07:32 +0100

    Create ex1-2.sh

---
+++ Activitat3/ex1-2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cont=0 #Contador que comptarà zeros
+
+for i in "$@"; do  #Per a cada arguments de la línea sencera "$@" fa: si és igual a 0, suma, sinó res i acaba
+	if [ $i -eq 0 ]; then
+    	cont=$((cont+1))
+	fi
+done
+
+echo "Hi han $cont zeros."  #Mostra el total de zeros
