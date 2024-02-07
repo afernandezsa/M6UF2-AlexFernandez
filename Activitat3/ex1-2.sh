b9109e17e4fd08a11addd50664b781abdb351934
alexfernandez <156780553+afernandezsa@users.noreply.github.com>
2024-02-07 21:26:08 +0100

    Update ex1-2.sh

--- Activitat3/ex1-2.sh
+++ Activitat3/ex1-2.sh
@@ -4,7 +4,7 @@ cont=0 #Contador que comptarà zeros
 
 for i in "$@"; do  #Per a cada arguments de la línea sencera "$@" fa: si és igual a 0, suma, sinó res i acaba
 	if [ $i -eq 0 ]; then
-    	cont=$((cont+1))
+    		cont=$((cont+1))
 	fi
 done
 
