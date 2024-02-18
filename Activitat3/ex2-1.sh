1c5b4c444f67a98e381990f2b482b172688eb7b5
alexfernandez <156780553+afernandezsa@users.noreply.github.com>
2024-02-18 12:30:24 +0100

    Update ex2-1.sh

--- Activitat3/ex2-1.sh
+++ Activitat3/ex2-1.sh
@@ -3,12 +3,12 @@
 arx=$1  #Agregi¡o variable per al paràmetre on passo l'arxiu
 
 if [ -e $arx ]; then  #Si l'arxiu es troba en la ruta, avisa i acaba
-   "L'arxiu $arx existeix"
+        echo"L'arxiu $arx existeix"
 fi
 
 echo "Escriu paraules a agregar, escriu ':>' per a parar" 
 while read -r parau && [ "$parau" != ":>" ]; do #Escriu paraules en cada linea amb "read -r" mentre sigui diferents a ":>", llavors va agregant paraula al arxiu passat
-  echo $parau >> $arx
+        echo $parau >> $arx
 done #Acaba bucle
 
 echo "S'han agregat les paraules al arxiu $arx"
